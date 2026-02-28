2b92c0b7e9a1a432019937fc8c2ed854cb35667b
Bajrangi kumar mishra <155826931+bajrangimishra9@users.noreply.github.com>
2026-03-01 02:53:05 +0530

    Delete src directory

--- src/pages/Login.tsx
+++
@@ -1,9 +0,0 @@
-import GoogleLoginButton from "../auth/GoogleLoginButton";
-
-export default function Login() {
-  return (
-    <div className="flex min-h-screen items-center justify-center">
-      <GoogleLoginButton />
-    </div>
-  );
-}
